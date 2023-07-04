84da3810cae5232a295b676b2135bde6064fd7e0
ahmadalkp <ahmadalkp@gmail.com>
2023-07-04 18:00:52 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { EcommerceComponent } from './pages/ecommerce/ecommerce.component';
 
 const routes: Routes = [
-  { path: 'ecommerce', component: EcommerceComponent },
+  { path: '', component: EcommerceComponent },
 ];
 
 @NgModule({
