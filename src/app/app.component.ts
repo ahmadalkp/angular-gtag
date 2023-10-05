e8a68cee1fa3205d42b2cd171a76b08c8ad7e75c
Ahmad.Kahfi <Ahmad.kahfi@astralife.co.id>
2023-10-05 11:43:35 +0700

    update script

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,29 @@ export class AppComponent {
   title = 'my-app-14';
   dataProperties : any = {};
 
+  ngOnInit() {
+    this.loadScript();
+  }
+
   click() {
     console.log(this.dataProperties);
+  }
+
+  loadScript() {
+    let node = document.createElement('script'); // creates the script tag
+    node.src = 'https://www.googletagmanager.com/gtag/js?id=G-TBX2CD8HSE'; // sets the source (insert url in between quotes)
+    node.type = 'text/javascript'; // set the script type
+    node.async = true; // makes script run asynchronously
+    // append to head of document
+
+    let node2 = document.createElement('script'); // creates the script tag
+    node2.innerHTML = `window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'G-TBX2CD8HSE');`
+
+    document.getElementsByTagName('head')[0].appendChild(node); 
+    document.getElementsByTagName('head')[0].appendChild(node2); 
   }
 }
